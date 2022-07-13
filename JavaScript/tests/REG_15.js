caa59e51cf0ba129531523ec9a0d022cc8ff533e
ScreimerTV <101062029+ScreimerTV@users.noreply.github.com>
2022-07-13 00:59:39 -0600

    Listo la automatizacion REG15
    Listo el script de automatizacion.

--- JavaScript/tests/REG_15.js
+++ JavaScript/tests/REG_15.js
@@ -9,12 +9,23 @@ async function pruebaReg15() {
     // Navegamos hasta el sitio web.
     await driver.get("https://www.pricesmart.com/site/cr/es/pagina-producto/382838");
 
-    //
+    // Se busca el elemento input para colocar cantidad de producto para pasarle un valor y luego perder el foco y obtener la validación.
     await driver.findElement(By.id("customValue")).sendKeys(300000, Key.TAB);
 
+    // Se busca el elemento que invoca el modal para seleccionar un club
     await driver.findElement(By.id("club-location-picker-modal")).click();
 
-    await driver.wait(until.elementLocated(By.id("select-club")), 30000).then(async () => { await driver.executeScript(`document.getElementById("select-club").click()`) });
+    // busca hasta que encuentra cualquier boton de cambiar club con un esperar explicito de 30 segundos
+    // luego si lo encuentra usar el retorno de la promesa para ejecutar javascript vanilla y dar click al boton
+    await driver.wait(until.elementLocated(By.id("select-club")), 30000)
+        .then(async () => {
+            await driver.executeScript(`document.getElementById("select-club").click()`)
+        });
+
+    await driver.findElement(By.id("customValue")).sendKeys(300000, Key.TAB);
+
+    //finaliza el caso de prueba.
+
 
 };
 
