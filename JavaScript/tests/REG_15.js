dfc7a483d02a7ad14b10b0bca9ca23cd40fcbb29
ScreimerTV <101062029+ScreimerTV@users.noreply.github.com>
2022-07-16 19:06:18 -0600

    actualizada con trycatch
    Version actualizada con TryCatch el REG_15

--- JavaScript/tests/REG_15.js
+++ JavaScript/tests/REG_15.js
@@ -1,12 +1,12 @@
 /* const { Builder, Browser, By, Key } = require('selenium-webdriver'); */
 const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 
-(async function pruebaReg15() {
+async function pruebaReg15() {
+    let error2 = null;
     // Ejecutamos el navegador
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
     try {
-
         // Navegamos hasta el sitio web.
         await driver.get("https://www.pricesmart.com/site/cr/es/pagina-producto/382838");
 
@@ -24,17 +24,18 @@ const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
             });
 
         await driver.findElement(By.id("customValue")).sendKeys(300000, Key.TAB);
-
-        //finaliza el caso de prueba.
     } catch (error) {
-        console.log("Existio un error", error);
-    } finally {
-        console.log("Debido al error se cerrara automaticamente el navegador");
-        await driver.quit();
+        console.log(error);
     }
 
 
-})();
+
+    //finaliza el caso de prueba.
+
+
+};
+
+pruebaReg15();
 
 
 
