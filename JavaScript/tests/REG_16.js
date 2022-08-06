78fcbe13bcfdfd6101d285350b6cd4002aab3d49
ScreimerTV <101062029+ScreimerTV@users.noreply.github.com>
2022-08-06 10:38:36 -0600

    Listo automatización
    Listo la Automatización.

--- JavaScript/tests/REG_16.js
+++ JavaScript/tests/REG_16.js
@@ -2,22 +2,30 @@
 const { Builder, Browser, By } = require("selenium-webdriver");
 
 (async function pruebaReg16() {
-    // Ejecutamos el navegador
-    // Ejecutamos el navegador
-    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  // Ejecutamos el navegador
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
-    await driver.get("https://www.pricesmart.com/site/hn/es");
+  //Se navega a la direccion del producto.
+  await driver.get(
+    "https://www.pricesmart.com/site/cr/es/pagina-producto/423554"
+  );
+  //Buscamos el elemento que agrega el producto al carrito
+  await driver.findElement(By.id("btn-add-to-cart")).click();
 
-    await driver.executeScript("window.scrollBy(0,document.body.scrollHeight)");
-
-    setTimeout(async () => {
-        await driver.executeScript(`document.querySelector("#footer > footer > div > div.row.py-4 > div > div > a > button").click()`);
-    }, 1000);
-
-    setTimeout(async () => {
-        await driver.executeScript(`document.querySelector("#country-picker-home > div > div:nth-child(5) > div > div > div:nth-child(1) > div > ul > li:nth-child(2) > a").click()`);
-    }, 1000);
-
-    setTimeout(async () => {
-        await driver.executeScript("window.scrollBy(0,document.body.scrollHeight)");
-    }, 1000);
+  // Buscamos el elemento que aumenta 1 a la cantidad de producto y se le da click. Se deberia actualizar la informacion de cantidad a pagar
+  await driver
+    .findElement(
+      By.xpath(
+        "/html/body/section/section/div/div/div[2]/div[1]/div/div/div/div[1]/div/div[3]/div[1]/div[2]/div/div[1]/form/div/div[2]/button"
+      )
+    )
+    .click();
+  // Buscamos el elemento que resta 1 a la cantidad de producto y se le da click. Se deberia actualizar la informacion de cantidad a pagar.
+  await driver
+    .findElement(
+      By.xpath(
+        "/html/body/section/section/div/div/div[2]/div[1]/div/div/div/div[1]/div/div[3]/div[1]/div[2]/div/div[1]/form/div/div[1]/button"
+      )
+    )
+    .click();
+})();
