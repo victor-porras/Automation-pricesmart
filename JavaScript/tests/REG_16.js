8e5f9b0ad7a612bd8a9407a69dbdec30183579dc
ScreimerTV <101062029+ScreimerTV@users.noreply.github.com>
2022-08-06 10:21:42 -0600

    Function change name
    Cambie los nombres de las funciones al correspondiente del nombre del archivo de la prueba.

---
+++ JavaScript/tests/REG_16.js
@@ -0,0 +1,23 @@
+/* const { Builder, Browser, By, Key } = require('selenium-webdriver'); */
+const { Builder, Browser, By } = require("selenium-webdriver");
+
+(async function pruebaReg16() {
+    // Ejecutamos el navegador
+    // Ejecutamos el navegador
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+    await driver.get("https://www.pricesmart.com/site/hn/es");
+
+    await driver.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+
+    setTimeout(async () => {
+        await driver.executeScript(`document.querySelector("#footer > footer > div > div.row.py-4 > div > div > a > button").click()`);
+    }, 1000);
+
+    setTimeout(async () => {
+        await driver.executeScript(`document.querySelector("#country-picker-home > div > div:nth-child(5) > div > div > div:nth-child(1) > div > ul > li:nth-child(2) > a").click()`);
+    }, 1000);
+
+    setTimeout(async () => {
+        await driver.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+    }, 1000);
