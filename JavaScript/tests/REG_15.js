2f3d2df65a4df635b9337509d973a95958f63eca
ScreimerTV <101062029+ScreimerTV@users.noreply.github.com>
2022-08-18 10:50:55 -0600

    actualice el indentado del REG_15
    Mejore el indentado del caso de prueba REG_15

--- JavaScript/tests/REG_15.js
+++ JavaScript/tests/REG_15.js
@@ -2,41 +2,39 @@
 const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 
 async function pruebaReg15() {
+  // Ejecutamos el navegador
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
-    // Ejecutamos el navegador
-    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  try {
+    // Navegamos hasta el sitio web.
+    await driver.get(
+      "https://www.pricesmart.com/site/cr/es/pagina-producto/382838"
+    );
 
-    try {
-        // Navegamos hasta el sitio web.
-        await driver.get("https://www.pricesmart.com/site/cr/es/pagina-producto/382838");
+    // Se busca el elemento input para colocar cantidad de producto para pasarle un valor y luego perder el foco y obtener la validación.
 
-        // Se busca el elemento input para colocar cantidad de producto para pasarle un valor y luego perder el foco y obtener la validación.
-        await driver.findElement(By.id("customValue")).sendKeys(300000, Key.TAB);
+    await driver.findElement(By.id("customValue")).sendKeys(300000, Key.TAB);
 
-        // Se busca el elemento que invoca el modal para seleccionar un club
-        await driver.findElement(By.id("club-location-picker-modal")).click();
+    // Se busca el elemento que invoca el modal para seleccionar un club
+    await driver.findElement(By.id("club-location-picker-modal")).click();
 
-        // busca hasta que encuentra cualquier boton de cambiar club con un esperar explicito de 30 segundos
-        // luego si lo encuentra usar el retorno de la promesa para ejecutar javascript vanilla y dar click al boton
-        await driver.wait(until.elementLocated(By.id("select-club")), 30000)
-            .then(async () => {
-                await driver.executeScript(`document.getElementById("select-club").click()`)
-            });
+    // busca hasta que encuentra cualquier boton de cambiar club con un esperar explicito de 30 segundos
+    // luego si lo encuentra usar el retorno de la promesa para ejecutar javascript vanilla y dar click al boton
+    await driver
+      .wait(until.elementLocated(By.id("select-club")), 30000)
+      .then(async () => {
+        await driver.executeScript(
+          `document.getElementById("select-club").click()`
+        );
+      });
 
-        await driver.findElement(By.id("customValue")).sendKeys(300000, Key.TAB);
-        //await driver.quit();
-    } catch (error) {
-        console.log(error);
-    }
+    await driver.findElement(By.id("customValue")).sendKeys(300000, Key.TAB);
+    //await driver.quit();
+  } catch (error) {
+    console.log(error);
+  }
 
-
-
-    //finaliza el caso de prueba.
-
-
-};
+  //finaliza el caso de prueba.
+}
 
 pruebaReg15();
-
-
-
